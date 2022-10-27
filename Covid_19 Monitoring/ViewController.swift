5f0ac0155b3bde7470be4618f0494453ab5931cd
Hugo Pinheiro <hugo_hp10@hotmail.com>
2022-10-26 22:18:09 -0300

    Initial Commit

---
+++ Covid_19 Monitoring/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Covid_19 Monitoring
+//
+//  Created by HP on 26/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
